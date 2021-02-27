745b9a864ad6eec19963a0301b2f95aa115c6649
NekoChan <aaron74327@gmail.com>
2021-02-26 21:17:36 -0800

    update

--- nHentai-Translator/app.js
+++ nHentai-Translator/app.js
@@ -79,24 +79,24 @@ function init () {
  */
 function nav (callback) {
     // 左側
-    for (let i = 1; i < Object.getOwnPropertyNames(json.menuLeft).length + 1; i++) {
-        $H(`.menu.left li:nth-child(${i}) > a`, json.menuLeft[Object.keys(json.menuLeft).sort((a, b)　=>　a - b)[i - 1]])
+    for (let i = 1; i < Object.getOwnPropertyNames(json.MenuLeft).length + 1; i++) {
+        $H(`.menu.left li:nth-child(${i}) > a`, json.MenuLeft[Object.keys(json.MenuLeft).sort((a, b)　=>　a - b)[i - 1]])
     }
 
     // 右側
     //  - 檢測是否有登入
     if (!/Sign in/.test($('.menu.right li:nth-child(1) >a').html())) {
         // 最愛
-        $H('.menu.right li:nth-child(1) > a', `<i class="fa fa-heart color-icon"></i> ${json.menuRight2.Favroites}`)
+        $H('.menu.right li:nth-child(1) > a', `<i class="fa fa-heart color-icon"></i> ${json.MenuRight2.Favroites}`)
         // 登出
-        $H('.menu.right li:nth-child(3) > a', `<i class="fa fa-sign-out-alt"></i> ${json.menuRight2.LogOut}`)
+        $H('.menu.right li:nth-child(3) > a', `<i class="fa fa-sign-out-alt"></i> ${json.MenuRight2.LogOut}`)
 
         login = true // 已登入
     } else {
         // 登入
-        $H('.menu.right li:nth-child(1) > a', `<i class="fa fa-sign-in-alt"></i> ${json.menuRight1.SignIn}`)
+        $H('.menu.right li:nth-child(1) > a', `<i class="fa fa-sign-in-alt"></i> ${json.MenuRight1.SignIn}`)
         // 註冊
-        $H('.menu.right li:nth-child(2) > a', `<i class="fa fa-edit"></i> ${json.menuRight1.Register}`)
+        $H('.menu.right li:nth-child(2) > a', `<i class="fa fa-edit"></i> ${json.MenuRight1.Register}`)
 
         login = false // 未登入
     }
@@ -108,10 +108,10 @@ function nav (callback) {
  */
 function homepage () {
     // 當前熱門
-    $H('#content .index-popular > h2', `<i class="fa fa-fire color-icon"></i> ${json.homepage.PopularNow}`)
+    $H('#content .index-popular > h2', `<i class="fa fa-fire color-icon"></i> ${json.Homepage.PopularNow}`)
 
     // 最新上傳
-    $H('#content .container:nth-child(3) > h2', `<i class="fa fa-box-tissue color-icon"></i> ${json.homepage.NewUploads}`)
+    $H('#content .container:nth-child(3) > h2', `<i class="fa fa-box-tissue color-icon"></i> ${json.Homepage.NewUploads}`)
 }
 
 /**
@@ -119,9 +119,9 @@ function homepage () {
  */
 function book () {
     // 左側標籤列表
-    for (let i = 1, span = ''; i < Object.getOwnPropertyNames(json.book.tagsName).length + 1; i++) {
+    for (let i = 1, span = ''; i < Object.getOwnPropertyNames(json.Book.TagsName).length + 1; i++) {
         span = $(`#tags > .tag-container:nth-child(${i}) > span`)[0].outerHTML
-        $H(`#tags > .tag-container:nth-child(${i})`, `${json.book.tagsName[Object.keys(json.book.tagsName).sort((a, b)=>a - b)[i - 1]]} ${span}`)
+        $H(`#tags > .tag-container:nth-child(${i})`, `${json.Book.TagsName[Object.keys(json.Book.TagsName).sort((a, b)=>a - b)[i - 1]]} ${span}`)
     }
 
     // 右側標籤列表
@@ -134,25 +134,25 @@ function book () {
         debugConsole(`頁數：${page}，確定大於 75 `)
 
         // 顯示更多
-        $H('#show-more-images-button', `<i class="fa fa-eye"></i> &nbsp; <span class="text">${json.book.ShowMoreImagesButton}</span>`)
+        $H('#show-more-images-button', `<i class="fa fa-eye"></i> &nbsp; <span class="text">${json.Book.ShowMoreImagesButton}</span>`)
 
         // 顯示全部
-        $H('#show-all-images-button', `<i class="fa fa-eye"></i> &nbsp; <span class="text">${json.book.ShowAllImagesButton}</span>`)
+        $H('#show-all-images-button', `<i class="fa fa-eye"></i> &nbsp; <span class="text">${json.Book.ShowAllImagesButton}</span>`)
     }
 
     // 更多類似的
-    $H('#related-container > h2', json.book.MoreLikeThis)
+    $H('#related-container > h2', json.Book.MoreLikeThis)
 
     // 留言
-    $H('#comment-post-container > h3', `<i class="fa fa-comments color-icon"></i> ${json.book.PostAComment}`)
+    $H('#comment-post-container > h3', `<i class="fa fa-comments color-icon"></i> ${json.Book.PostAComment}`)
     if (login) {
         // 如果你詢問是否有翻譯，你將會死亡。
-        $('#comment_form > textarea').attr('placeholder',`${json.book.CommentFormPlaceHolder}`)
+        $('#comment_form > textarea').attr('placeholder',`${json.Book.CommentFormPlaceHolder}`)
         // 發送
-        $H('#comment_form > div > button', `<i class="fa fa-comment"></i> ${json.book.Comment}`)
+        $H('#comment_form > div > button', `<i class="fa fa-comment"></i> ${json.Book.Comment}`)
     } else {
         // 登入或 註冊 和其他基友一起討論。
-        $H('#comment-post-container > div > p', `<a class="login-comment" href="/login/">${json.book.NoLogin.Login}</a> ${json.book.NoLogin.Or} <a class="login-comment" href="/register/">${json.book.NoLogin.Register}</a> ${json.book.NoLogin.ToPostAComment}`)
+        $H('#comment-post-container > div > p', `<a class="login-comment" href="/login/">${json.Book.NoLogin.Login}</a> ${json.Book.NoLogin.Or} <a class="login-comment" href="/register/">${json.Book.NoLogin.Register}</a> ${json.Book.NoLogin.ToPostAComment}`)
     }
 
     // 時間
@@ -204,17 +204,17 @@ function tagsTranslator (tags) {
  */
 function timeTranslator (time) {
     return time
-        .replace('years', json.book.Time.Years)
-        .replace('year', json.book.Time.Year)
-        .replace('months', json.book.Time.Months)
-        .replace('month', json.book.Time.Month)
-        .replace('day', json.book.Time.Day)
-        .replace('days', json.book.Time.Days)
-        .replace('hours', json.book.Time.Hours)
-        .replace('hour', json.book.Time.Hour)
-        .replace('minutes', json.book.Time.Minutes)
-        .replace('minute', json.book.Time.Minute)
-        .replace('seconds', json.book.Time.Seconds)
-        .replace('second', json.book.Time.Second)
-        .replace('ago', json.book.Time.Ago)
+        .replace('years', json.Book.Time.Years)
+        .replace('year', json.Book.Time.Year)
+        .replace('months', json.Book.Time.Months)
+        .replace('month', json.Book.Time.Month)
+        .replace('day', json.Book.Time.Day)
+        .replace('days', json.Book.Time.Days)
+        .replace('hours', json.Book.Time.Hours)
+        .replace('hour', json.Book.Time.Hour)
+        .replace('minutes', json.Book.Time.Minutes)
+        .replace('minute', json.Book.Time.Minute)
+        .replace('seconds', json.Book.Time.Seconds)
+        .replace('second', json.Book.Time.Second)
+        .replace('ago', json.Book.Time.Ago)
 }
