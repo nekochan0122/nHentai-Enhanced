dd6485bcefd1f18da6fc7d9c9eb70a2c4a6fe10d
NekoChan <aaron74327@gmail.com>
2021-03-19 22:13:25 -0700

    Update app.js

--- nHentai-Enhanced/app.js
+++ nHentai-Enhanced/app.js
@@ -288,8 +288,25 @@ function book () {
     }
     // 插入按鈕元素
     $('#info > .buttons').append(`<a href="/search/?q=${searchText}" id="serachRelatedBook" class="btn btn-secondary"><i class="fas fa-search"></i> ${json.Book.btn.serachRelatedBook} (<span id="resultNum">讀取中...</span>)</a>`)
-    // 獲取搜尋結果數量
-    ajaxSerachRelatedBook(searchText)
+    // 獲取搜尋結果數量並修改
+    $.ajax({
+        type: "GET",
+        url: `/search/?q=${searchText}`,
+        dataType: "html",
+        success: data => {
+            debugConsole(`搜尋 ${searchText} 獲取成功`)
+
+            let newHtml = $('<div></div>'),
+                resultNum = newHtml.html(data).find('#content > h1').text().replace('results', '')
+
+            $H('#resultNum', resultNum)
+
+            resultNum === 0 ? $('#serachRelatedBook').hide() : null
+        },
+        error: () => {
+            debugConsole(`搜尋 ${searchText} 獲取失敗`)
+        }
+    })
 
     // 偵測頁數 & 按紐
     const page = $('.thumb-container').length
@@ -363,31 +380,6 @@ function ajaxNextPage (mode) {
         },
         error: () => {
             debugConsole(`第${currentPageNum}頁 獲取失敗`)
-        }
-    })
-}
-
-/**
- * 搜尋相關本本的結果
- * @param {string} searchText - 搜尋的內容
- */
-function ajaxSerachRelatedBook (searchText) {
-    $.ajax({
-        type: "GET",
-        url: `/search/?q=${searchText}`,
-        dataType: "html",
-        success: data => {
-            debugConsole(`搜尋 ${searchText} 獲取成功`)
-
-            let newHtml = $('<div></div>'),
-                resultNum = newHtml.html(data).find('#content > h1').text().replace('results', '')
-
-            $H('#resultNum', resultNum)
-
-            resultNum === 0 ? $('#serachRelatedBook').hide() : null
-        },
-        error: () => {
-            debugConsole(`搜尋 ${searchText} 獲取失敗`)
         }
     })
 }
