065c35d83317b5f8535ac680142f22a50c4187d0
NekoChan <aaron74327@gmail.com>
2021-03-22 06:08:58 -0700

    Update app.js

--- nHentai-Enhanced/app.js
+++ nHentai-Enhanced/app.js
@@ -501,7 +501,6 @@ function ajaxNextPage (mode) {
             // 黑名單 class 加 blacklisted
             // Code from：https://static.nhentai.net/js/scripts.8d76ecfad261.js
             const t = window.n.options.blacklisted_tags
-            if (t && t.length)
             for (let e = t.map((t => {
                 return ".tag-".concat(t, ',.gallery[data-tags~="').concat(t, '"]')
             })).join(","), n = document.querySelectorAll(e), r = 0; r < n.length; r++) n[r].classList.add("blacklisted")
