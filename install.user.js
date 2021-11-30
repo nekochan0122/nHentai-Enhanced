97d6a70a801dde1ef47f32f9eec7b49ed19e2e39
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2021-11-29 17:26:35 -0800

    Create install.user.js

---
+++ install.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name       nHentai-Enhanced
+// @version    0.0.1
+// @author     NekoChan
+// @homepage   https://github.com/NekoChanTaiwan/nHentai-Enhanced
+// @supportURL https://github.com/NekoChanTaiwan/nHentai-Enhanced/issues
+// @match      https://nhentai.net/*
+// @namespace  https://github.com/NekoChanTaiwan
+// @license    MIT
+// @grant      none
+// @require    https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
+// @require    https://cdn.jsdelivr.net/npm/clipboard@2.0.8/dist/clipboard.min.js
+// @require    https://cdn.jsdelivr.net/npm/notyf@3.9.0/notyf.min.js
+// @require    https://cdn.jsdelivr.net/gh/821938089/finder@2.0.0.1/finder.js
+// @require    https://github.com/NekoChanTaiwan/nHentai-Enhanced/raw/main/dist/app.user.js
+// ==/UserScript==
